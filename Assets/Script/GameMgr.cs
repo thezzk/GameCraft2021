9fcc940f995f525c36ca06a711b8a885b64c6c3a
simshengqin <sqsim.2018@sis.smu.edu.sg>
2021-01-09 00:04:03 +0800

    Add goldcoin and coinrush

--- Assets/Script/GameMgr.cs
+++ Assets/Script/GameMgr.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class GameMgr : MonoBehaviour
 {
-    [SerializeField] GameObject coinPref;
     [SerializeField] Terrain genTerrain;
+    [SerializeField] GameObject coinPref;
     [SerializeField] int maxCoin;
     [SerializeField] GameObject coinGenPoints;
+    [SerializeField] GameObject goldCoinPref;
+    [SerializeField] int maxGoldCoin;
+    [SerializeField] GameObject goldCoinGenPoints;
     [SerializeField] GameObject healthPackPref;
     [SerializeField] int maxHealthPack;
     [SerializeField] GameObject healthPackGenPoints;
@@ -16,22 +19,57 @@ public class GameMgr : MonoBehaviour
     [HideInInspector] public float currentGameTime;
 
     private int coinCnt = 0;
+    private int goldCoinCnt = 0;
+    public int goldCoinDelay2 = 10;
+    public int coinRushDelay = Random.Range(10,30);
     private int healthPackCnt = 0;
 
 
     IEnumerator waitAndGenCoin()
     {
+        coinRushDelay = Random.Range(10,30);
         while (gameRunning)
         {
             yield return new WaitForSeconds(1f);
-            if (coinCnt < maxCoin)
+            if (coinRushDelay == 0 ) {
+                Debug.Log(coinRushDelay);
+                Debug.Log("Coin Rush!!");
+                int i = 0;
+                while (i < 30) {
+                    int genPointIndex = Random.Range(0, coinGenPoints.transform.childCount);
+                    Transform genPointTran = coinGenPoints.transform.GetChild(genPointIndex);
+                    var coin = Instantiate(coinPref, genPointTran.position, Quaternion.identity);
+                    coin.transform.GetChild(0).GetComponent<Coin>().onGainedCoin += DecCoinCnt;
+                    coinCnt++; 
+                    i++;                   
+                }
+                   
+            }            
+            else if (coinCnt < maxCoin)
             {
+
                 int genPointIndex = Random.Range(0, coinGenPoints.transform.childCount);
                 Transform genPointTran = coinGenPoints.transform.GetChild(genPointIndex);
                 var coin = Instantiate(coinPref, genPointTran.position, Quaternion.identity);
                 coin.transform.GetChild(0).GetComponent<Coin>().onGainedCoin += DecCoinCnt;
                 coinCnt++;
             }
+            if (goldCoinDelay2 == 0 && goldCoinCnt < maxGoldCoin)
+            {
+                int genPointIndex = Random.Range(0, goldCoinGenPoints.transform.childCount);
+                Transform genPointTran = goldCoinGenPoints.transform.GetChild(genPointIndex);
+                var goldCoin = Instantiate(goldCoinPref, genPointTran.position, Quaternion.identity);
+                goldCoin.transform.GetChild(0).GetComponent<GoldCoin>().onGainedCoin += DecGoldCoinCnt;
+                goldCoinCnt++;
+                
+            }
+            if (goldCoinDelay2 == 0) {
+                goldCoinDelay2 = 10;
+            }
+            if (coinRushDelay == 0) {
+                coinRushDelay = Random.Range(10,30);
+            }
+            
             if (healthPackCnt < maxHealthPack)
             {
                 int genPointIndex = Random.Range(0, healthPackGenPoints.transform.childCount);
@@ -40,6 +78,10 @@ public class GameMgr : MonoBehaviour
                 healthPack.GetComponent<HealthPack>().onGainedHealthPack += DecHealthPackCnt;
                 healthPackCnt++;
             }
+            goldCoinDelay2--;
+            //Debug.Log(goldCoinDelay2);
+            coinRushDelay--;
+            Debug.Log(coinRushDelay);
 
         }
     }
@@ -48,6 +90,11 @@ public class GameMgr : MonoBehaviour
     {
         Debug.Log("coin --");
         coinCnt--;
+    }
+    public void DecGoldCoinCnt()
+    {
+        Debug.Log("Gold coin --");
+        goldCoinCnt--;
     }
 
     private void DecHealthPackCnt()
---
+++ Assets/Script/GoldCoin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldCoin : MonoBehaviour
+{
+    public event Action onGainedCoin;
+    [SerializeField] public Animator anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            onGainedCoin();
+            Destroy(this.gameObject);
+            other.GetComponent<PlayerControl>().gainGoldCoin();
+        }
+    }
+
+    public void startSpinning()
+    {
+        anim.SetBool("spin", true);
+    }
+
+}
