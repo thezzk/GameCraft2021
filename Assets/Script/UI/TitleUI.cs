61c19101d3fc973ad2c5c9cce6069573f0ce65e7
LAPTOP-EN852T29\zzk <thezzk@qq.com>
2021-01-09 11:48:24 +0800

    Add title UI

---
+++ Assets/Script/UI/TitleUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleUI : MonoBehaviour
+{
+    
+    [SerializeField] Canvas gamingUI;
+    private GameMgr gameMgr;
+
+    private void Start() 
+    {
+        gameMgr = FindObjectOfType<GameMgr>();
+    }
+
+    private void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            gamingUI.gameObject.SetActive(true);
+            gameObject.SetActive(false);
+            gameMgr.gameRunning = true;
+        }
+    }
+}
